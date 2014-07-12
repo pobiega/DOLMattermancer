96e9bf935bc23cc0819083e290c0fd846ce6de81
pobiega <robin.aakerman@gmail.com>
2014-07-12 16:16:28 +0200

    Stacking DoTs added.
    Database spell type is "StackingDamageOverTime". Spell.Value is used for
    max stacks - everything else is as normal DamageOverTime.

---
+++ mattermancer/StackingDoTSpellEffect.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Reflection;
+using System.Text;
+
+using DOL.Database;
+using DOL.GS.Spells;
+using DOL.GS.PacketHandler;
+using DOL.Language;
+
+using log4net;
+using System.Collections.Generic;
+
+namespace DOL.GS.Effects
+{
+    public class StackingDoTSpellEffect : GameSpellEffect
+    {
+        /// <summary>
+        /// Defines a logger for this class.
+        /// </summary>
+        private static readonly ILog log = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
+        private readonly object m_LockObject = new object(); // dummy object for thread sync - Mannen
+
+        public StackingDoTSpellEffect(ISpellHandler handler, int duration, int pulseFreq, double effectiveness)
+            : base(handler,duration,pulseFreq,effectiveness)
+        {
+        }
+
+        /// <summary>
+		/// Creates a new game spell effect
+		/// </summary>
+		/// <param name="handler">the spell handler</param>
+		/// <param name="duration">the spell duration in milliseconds</param>
+		/// <param name="pulseFreq">the pulse frequency in milliseconds</param>
+        public StackingDoTSpellEffect(ISpellHandler handler, int duration, int pulseFreq)
+            : this(handler, duration, pulseFreq, 1)
+		{
+		}
+
+
+
+        public override void Overwrite(GameSpellEffect effect)
+        {
+            StackingDoTSpellEffect sdotEffect = effect as StackingDoTSpellEffect;
+            if (sdotEffect == null)
+                return;
+
+            if (Spell.Concentration > 0)
+            {
+                if (log.IsWarnEnabled)
+                    log.Warn(effect.Name + " (" + effect.Spell.Name + ") is trying to overwrite " + Spell.Name + " which has concentration " + Spell.Concentration);
+                return;
+            }
+
+            lock (m_LockObject)
+            {
+                StopTimers();
+                //m_handler = effect.m_handler;
+                // instead of replacing the handler, increase the stackcounter.
+
+                StackingDoTSpellHandler sdotHandler = m_handler as StackingDoTSpellHandler;
+                if (sdotHandler == null)
+                    return;
+                
+                sdotHandler.StackCount++;
+
+                m_duration = sdotEffect.m_duration;
+                m_pulseFreq = sdotEffect.m_pulseFreq;
+                m_effectiveness = sdotEffect.m_effectiveness;
+                StartTimers();
+                if (Spell.Concentration > 0)
+                {
+                    SpellHandler.Caster.ConcentrationEffects.Add(this);
+                }
+                m_owner.EffectList.OnEffectsChanged(this);
+                m_handler.OnEffectStart(this);
+                m_handler.OnEffectPulse(this);
+                m_expired = false;
+            }
+        }
+    }
+}
---
+++ mattermancer/StackingDoTSpellHandler.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using DOL.GS;
+using DOL.GS.PacketHandler;
+using DOL.GS.Effects;
+using DOL.Language;
+using DOL.AI.Brain;
+
+namespace DOL.GS.Spells
+{
+    [SpellHandlerAttribute("StackingDamageOverTime")]
+    class StackingDoTSpellHandler : DoTSpellHandler
+    {
+        public StackingDoTSpellHandler(GameLiving caster, Spell spell, SpellLine spellLine)
+            : base(caster, spell, spellLine)
+        {
+        }
+
+        private int m_stackCount = 1;
+        public int StackCount
+        {
+            get { return m_stackCount; }
+            set
+            {
+                m_stackCount = value;
+                if (m_stackCount > (int)this.m_spell.Value)
+                    m_stackCount = (int)this.m_spell.Value;
+
+                MessageToCaster("StackCount is now " + m_stackCount.ToString(), eChatType.CT_Staff);
+            }
+        }
+
+        protected override GameSpellEffect CreateSpellEffect(GameLiving target, double effectiveness)
+        {
+            return new StackingDoTSpellEffect(this, m_spell.Duration, m_spell.Frequency, effectiveness);
+        }
+
+        public override AttackData CalculateDamageToTarget(GameLiving target, double effectiveness)
+        {
+            AttackData ad = base.CalculateDamageToTarget(target, effectiveness);
+
+            ad.Damage *= StackCount;
+
+            return ad;
+        }
+    }
+}
